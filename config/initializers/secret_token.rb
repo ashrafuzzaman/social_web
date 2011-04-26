d99e55ff0b7db55aaa78791ae5f5d7d2e938e12e
A.K.M. Ashrafuzzaman <jitu@dragon.cust.tele2.se>
2011-04-26 02:10:06 -0400

    Uploaded the code in github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwIdentity::Application.config.secret_token = 'c8467e1945f346feaaae59786daea696202666b436ba9263e771f9f76d6c7123a478525766804c14776fdfc13d6bab2d1411a5bddae261f2780713639d24d5e1'
