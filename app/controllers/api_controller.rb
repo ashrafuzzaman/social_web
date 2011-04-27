922a1694b0b65d81c93acbda141ea59de17d3df8
A.K.M. Ashrafuzzaman <jitu@dragon.cust.tele2.se>
2011-04-27 17:15:51 -0400

    Added !

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   def register
     logger.info "User Info :: #{params[:email]} #{params[:password]}"
-    @user = User.create(:email => params[:email], :password => params[:password])
+    @user = User.create!(:email => params[:email], :password => params[:password])
     render :json => @user
   end
 
