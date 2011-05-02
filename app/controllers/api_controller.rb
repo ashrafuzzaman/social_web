7b4438029fae9e561620c7934d15f67e366e8533
A.K.M. Ashrafuzzaman <jitu@dragon.cust.tele2.se>
2011-05-02 11:03:40 -0400

    Fixed the error message

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,12 @@
 class ApiController < ApplicationController
   def user_register
     logger.info "User Info :: #{params[:email]} #{params[:password]}"
-    @user = User.create!(:email => params[:email], :password => params[:password])
-    render :json => @user
+    @user = User.create(:email => params[:email], :password => params[:password])
+
+    if @user.valid?
+      render :json => @user
+    else
+      render :json => {:error => @user.errors.full_messages}
+    end
   end
 end
