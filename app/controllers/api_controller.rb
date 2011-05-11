79b8ef8c6438aa32301e2021ac8d50a2fbebb727
A.K.M. Ashrafuzzaman <jitu@dragon.cust.tele2.se>
2011-05-11 13:43:17 -0400

    added default datastore

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,7 +5,7 @@ class ApiController < ApplicationController
   #curl -d "email=test@test.com&password=123456" http://localhost:3000/api/users.json
   def user_register
     logger.info "User Info :: #{params[:email]} #{params[:password]}"
-    @user = User.create(:email => params[:email], :password => params[:password])
+    @user = User.create(:email => params[:email], :password => params[:password], :data_service_host => "http://social-web-data.heroku.com")
 
     if @user.valid?
       @user.reset_authentication_token!
