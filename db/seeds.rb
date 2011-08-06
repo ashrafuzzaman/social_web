dd874b9ed0ad39f8fb72828dc376edc044de80d8
unknown <jitu@.(none)>
2011-08-07 00:12:43 +0600

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+User.delete_all
+User.create(:email => "ashrafuzzaman.g2@gmail.com", :password => "123456", :data_service_host => 'http://10.0.2.2:3001')
+User.create(:email => "zmn.ashraf@gmail.com", :password => "123456", :data_service_host => 'http://10.0.2.2:3001')
