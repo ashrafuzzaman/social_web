9a409372b6e68eb32582a72a4b049e369981f321
unknown <jitu@.(none)>
2011-08-15 16:33:04 +0600

    Fixed the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+#data_store = 'http://10.0.2.2:3001'
+data_store = 'http://social-web-data.heroku.com'
+
 User.delete_all
-User.create(:email => "ashrafuzzaman.g2@gmail.com", :password => "123456", :data_service_host => 'http://10.0.2.2:3001')
-User.create(:email => "zmn.ashraf@gmail.com", :password => "123456", :data_service_host => 'http://10.0.2.2:3001')
+User.create(:email => "ashrafuzzaman.g2@gmail.com", :password => "123456", :data_service_host => data_store)
+User.create(:email => "zmn.ashraf@gmail.com", :password => "123456", :data_service_host => data_store)
